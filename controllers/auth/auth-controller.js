57e8b3c24077205197144994f3348457f196b267
hollypraisy <lapianholly@gmail.com>
2025-08-16 09:43:55 +0800

    update set cookie token

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -15,14 +15,18 @@ const generateToken = (user) => jwt.sign({
   phoneNumber: user.phoneNumber,
 }, JWT_SECRET, { expiresIn: "60m" });
 
-const setTokenCookie = (res, token) => {
+setTokenCookie = (res, token) => {
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,                     
+    secure: isProduction,            
+    sameSite: isProduction ? "None" : "Lax",  
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
+
+
 
 // === Auth ===
 const registerUser = async (req, res) => {
--- server.js
+++ server.js
@@ -32,7 +32,6 @@ const PORT = process.env.PORT || 5000;
 // ✅ CORS Setup
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://localhost:4173",
   "https://walekreasi.vercel.app",
 ];
 
